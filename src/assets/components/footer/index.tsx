b9f423fea678c21cb55714150489176607e97a41
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-08-15 19:56:59 -0300

    Logo CPS

--- src/assets/components/footer/index.tsx
+++ src/assets/components/footer/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import '../../styles/bootstrap.css';
 import '../../styles/global.css';
+import logoCPS from '../../images/cps-logo.png';
 
 export default function Footer() {
     return (
         <div className="footer">
             <footer id="main-footer" className="py-5 bg-black">
                 <div className="container">
-                    <img src="" alt="Centro Paula Souza"/>
+                    <img src={logoCPS} className="logoCPS" alt="Centro Paula Souza"/>
                     <p className="m-0 text-center text-black">Copyright &copy; Fatec Guarulhos 2020</p>
                 </div>
             </footer>
